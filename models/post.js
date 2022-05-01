7ea9df69580b285e2efe5afc53946df8d7a4aaec
Feisal Mohamed <feisalmib@gmail.com>
2022-05-02 00:26:39 +0300

    models

---
+++ models/post.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  title: {
+    type: String,
+    require: true,
+  },
+  body: {
+    type: String,
+    require: true,
+  },
+  author: {
+    // name: { type: String,required:true },
+    // authorId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+
+  likes: {
+    type: Number,
+  },
+  //   },
+});
+
+PostSchema.methods.addLike = function () {
+  this.likes += 1;
+  return this.likes;
+};
+
+PostSchema.methods.addDislike = function () {
+  if (this.likes >= 1 ) {
+    this.likes -= 1;
+  }
+  return this.likes;
+};
+
+PostSchema.methods.getLikes = function () {
+  return this.likes;
+};
+
+
+PostSchema.methods.getExtract=function(){
+  const maxLength= 250;
+  return this.body.substr(0, maxLength);
+}
+module.exports = mongoose.model("Post", PostSchema);
